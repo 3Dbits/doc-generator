869f5932e96b7145ef00c6926cc5f21ddd5b77e8
Matija Cvetan <matija.cvetan28@gmail.com>
2025-01-08 10:08:59 +0100

    Unified dependency versioning by removing springVersion in favor of using kotlinVersion for both

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,13 @@ rootProject.name = "doc-generator"
 
 pluginManagement {
     val kotlinVersion: String by settings
-    val springVersion: String by settings
     val springBootVersion: String by settings
     val springDependencyManagementVersion: String by settings
     val ktlintVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
-        kotlin("plugin.spring") version springVersion
+        kotlin("plugin.spring") version kotlinVersion
         id("org.springframework.boot") version springBootVersion
         id("io.spring.dependency-management") version springDependencyManagementVersion
         id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
